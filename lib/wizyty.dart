97f46f4f55569fc77ea15b257892792b2082efa0
Paulina Rogala <rogala@student.agh.edu.pl>
2026-06-30 11:28:30 +0200

    dodanie wizyty

---
+++ lib/wizyty.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Wizyty extends StatefulWidget {
+  const Wizyty({super.key});
+
+  @override
+  State<Wizyty> createState() {
+    return _WizytyState();
+  }
+}
+
+class _WizytyState extends State<Wizyty> {
+  @override
+  Widget build(BuildContext context) {
+    int myIndex = 4;
+    return Scaffold(
+      appBar: AppBar(title: const Text("Wizyty lekarskie")),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Stack(
+      children: [
+        Container(
+          child: TableCalendar<dynamic>(
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2035, 10, 16),
+            focusedDay: DateTime.now(),
+          ),
+        ),
+      ],
+    );
+  }
+}
